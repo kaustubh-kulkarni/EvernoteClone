30bcd77559a7f60ffb6e1c4c51f08ea5244db78f
Kaustubh Kulkarni <kulkarnikaustubhv@gmail.com>
2022-02-25 20:50:23 +0100

    Initial skeleton for project

---
+++ App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Evernote.Views;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Evernote
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            LoadLoader();
+        }
+
+        //Load the loader
+        private static void LoadLoader()
+        {
+            Loader loader = new Loader();
+            loader.Show();
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Thread.Sleep(2500);
+            stopwatch.Stop();
+
+            loader.Hide();
+        }
+    }
+}
